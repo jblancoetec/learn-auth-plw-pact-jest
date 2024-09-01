9dcf2200cd6845183c871a750051e8fa5ab8d28e
Joaquin Blanco <blancojoaquin77@gmail.com>
2024-08-31 22:48:05 -0300

    refactor: elimine una factory que no estaba utilizando

--- app/api/users/sign-in/controller/index.ts
+++ app/api/users/sign-in/controller/index.ts
@@ -1,22 +1,9 @@
-import { ParseRequestResult } from "../parser";
 import { parseRequest } from "../parser";
-import { signInUser, SignInUserProps, SignInUserResult } from "../repo";
+import { signInUser, SignInUserResult } from "../repo";
 
-type SignInUserIfRequestIsValid = (request: any) => Promise<SignInUserResult>;
-type ParseRequest = (request: any) => ParseRequestResult;
-type SignInUser = (props: SignInUserProps) => Promise<SignInUserResult>;
-
-export const signInUserIfRequestIsValidFactory = (
-  parseRequest: ParseRequest,
-  signInUser: SignInUser,
-): SignInUserIfRequestIsValid => {
-  return async (request: any): Promise<SignInUserResult> => {
-    const { parsed, message, data } = parseRequest(request);
-    return parsed ? await signInUser(data!) : { state: "not-parsed", message };
-  };
+export const signInUserIfRequestIsValid = async (
+  request: any,
+): Promise<SignInUserResult> => {
+  const { parsed, message, data } = parseRequest(request);
+  return parsed ? await signInUser(data!) : { state: "not-parsed", message };
 };
-
-export const signInUserIfRequestIsValid = signInUserIfRequestIsValidFactory(
-  parseRequest,
-  signInUser,
-);
